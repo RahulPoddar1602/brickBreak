95a813524e884d193f6fbcd4a250942b4a9d788c
RahulPoddar1602 <rahul1999p@gmail.com>
2023-01-04 17:00:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+import Game from "./game.js";
+const canvas =document.getElementById("gameScreen");
+const ctx = canvas.getContext("2d");
+
+// ctx.clearRect(0,0,800,600);
+const GAME_WIDTH= 800;
+const GAME_HEIGHT= 600;
+let game = new Game(GAME_WIDTH,GAME_HEIGHT);
+// game.start();
+
+let lastTime =0;
+
+function gameLoop(timeStamp){
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+
+    game.update(deltaTime);
+    game.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+requestAnimationFrame(gameLoop);
